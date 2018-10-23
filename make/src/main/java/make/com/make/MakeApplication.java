160838fe22054975ab772661dfbb53ee554c786b
KELLY <kellbianca0@gmail.com>
2018-10-23 13:36:03 -0300

    Primeiro Commit

---
+++ make/src/main/java/make/com/make/MakeApplication.java
@@ -0,0 +1,12 @@
+package make.com.make;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeApplication.class, args);
+	}
+}
