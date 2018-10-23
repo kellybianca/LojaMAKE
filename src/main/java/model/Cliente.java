0c3eef069b9dca6c25f4d23d1ab44e250fbc7514
KELLY <kellbianca0@gmail.com>
2018-10-23 14:26:43 -0300

    Criacao de classe cliente, mapeamento e get e set

---
+++ src/main/java/model/Cliente.java
@@ -0,0 +1,59 @@
+package model;
+import javax.persistence.*;
+
+@Entity
+public class Cliente {
+
+	@Id
+	@GeneratedValue(strategy =GenerationType.AUTO)
+	private Long id;
+	
+	@Column (nullable = false)
+	private String nome;
+	@Column (nullable = false, unique=true)
+	private  String cpf;
+	
+	@Column(nullable = false)
+	private String senha;
+	
+	@OneToOne(
+			cascade = CascadeType.ALL,
+			orphanRemoval = true
+			)
+	
+	private Carrinho;
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	
+	
+	  
+	
+}
