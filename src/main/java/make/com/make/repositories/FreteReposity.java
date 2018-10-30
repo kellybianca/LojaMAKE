bdae65eafe1c40735075bee34682934beaa7de88
Kelly <kellbianca0@gmail.com>
2018-10-30 13:38:49 -0300

    Criação da interface FreteRepository

---
+++ src/main/java/make/com/make/repositories/FreteReposity.java
@@ -0,0 +1,9 @@
+package make.com.make.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import make.com.make.model.Frete;
+
+public interface FreteReposity extends JpaRepository<Frete, Long>{
+
+}
