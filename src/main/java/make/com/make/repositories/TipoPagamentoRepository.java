72f80add0f9a72c74a33942ea0bbfd5f2931c81d
Maria Luana <mluanasantos157@gmail.com>
2018-10-30 13:43:49 -0300

    Criação da interface TipoPagamentoRepository

---
+++ src/main/java/make/com/make/repositories/TipoPagamentoRepository.java
@@ -0,0 +1,9 @@
+package make.com.make.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import make.com.make.model.TipoPagamento;
+
+public interface TipoPagamentoRepository extends JpaRepository<TipoPagamento,Long> {
+
+}
