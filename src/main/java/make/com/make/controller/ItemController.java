c4fcb717284396df234eeb08f62002b139b555bc
Kelly, Maria, Erica <erickabeatryzs2@gmail.com>
2018-11-01 15:50:24 -0300

    Pacote apagado na classe ClienteController e classe itemController adicionada

---
+++ src/main/java/make/com/make/controller/ItemController.java
@@ -0,0 +1,57 @@
+package make.com.make.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import make.com.make.exception.ResourceNotFoundException;
+import make.com.make.model.Item;
+import make.com.make.repositories.ItemRepository;
+
+@RestController
+public class ItemController {
+
+	@Autowired
+	private ItemRepository itemRepository;
+	
+	@GetMapping("/item")
+	public Page<Item> getItem(Pageable pageable){
+		return itemRepository.findAll(pageable);
+	}
+	
+	@PostMapping("/item")
+	public Item createItem(@Valid @RequestBody Item item) {
+		return itemRepository.save(item);
+	}
+	
+	@PutMapping("/item/{itemId}")
+	public Item updateItem(@PathVariable Long itemId,
+								@Valid @RequestBody Item itemRequest) {
+		return itemRepository.findById(itemId)
+				.map(item -> {
+					item.setProduto(itemRequest.getProduto());
+					item.setQuantidade(itemRequest.getQuantidade());
+					item.setValor(itemRequest.getValor());
+					return itemRepository.save(item);
+				}).orElseThrow(() -> new ResourceNotFoundException("item not found: "+itemId));
+	}
+	
+	@DeleteMapping("/item/{itemId}")
+	public ResponseEntity<?> deleteQuestion(@PathVariable Long itemId){
+		return itemRepository.findById(itemId)
+				.map(item -> {
+					itemRepository.delete(item);
+					return ResponseEntity.ok().build();
+				}).orElseThrow(() -> new ResourceNotFoundException("Item not found: "+itemId));
+	}
+}
